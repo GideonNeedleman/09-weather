f551bb39cbd22ac6fe8831c214b7b276965daee1
Gideon Needleman <gideon.works@zohomail.com>
2023-10-04 13:13:35 -0700

    refactor code into multiple files

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,10 @@
 import { useEffect, useState } from "react";
-
-function getWeatherIcon(wmoCode) {
-  const icons = new Map([
-    [[0], "☀️"],
-    [[1], "🌤"],
-    [[2], "⛅️"],
-    [[3], "☁️"],
-    [[45, 48], "🌫"],
-    [[51, 56, 61, 66, 80], "🌦"],
-    [[53, 55, 63, 65, 57, 67, 81, 82], "🌧"],
-    [[71, 73, 75, 77, 85, 86], "🌨"],
-    [[95], "🌩"],
-    [[96, 99], "⛈"],
-  ]);
-  const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
-  if (!arr) return "NOT FOUND";
-  return icons.get(arr);
-}
-
-function convertToFlag(countryCode) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
-}
-
-function formatDay(dateStr) {
-  return new Intl.DateTimeFormat("en", {
-    weekday: "short",
-  }).format(new Date(dateStr));
-}
+import Weather from "./Weather";
+import { convertToFlag } from "../utils/helpers";
 
 export default function App() {
   const [location, setLocation] = useState("");
   const [altLocations, setAltLocations] = useState([]);
-  const [altChosen, setAltChosen] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const [displayLocation, setDisplayLocation] = useState("");
@@ -138,44 +107,5 @@ export default function App() {
         <Weather weather={weather} location={displayLocation} />
       )}
     </div>
-  );
-}
-
-function Weather({ weather, location }) {
-  const {
-    temperature_2m_max: max,
-    temperature_2m_min: min,
-    time: dates,
-    weathercode: codes,
-  } = weather;
-  console.log(weather);
-  return (
-    <div>
-      <h2>Forecast for {location}</h2>
-      <ul className="weather">
-        {dates?.map((date, i) => (
-          <Day
-            date={date}
-            max={max.at(i)}
-            min={min.at(i)}
-            code={codes.at(i)}
-            key={date}
-            isToday={i === 0}
-          />
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-function Day({ date, max, min, code, isToday }) {
-  return (
-    <li className="day">
-      <span>{getWeatherIcon(code)}</span>
-      <p>{isToday ? "Today" : formatDay(date)}</p>
-      <p>
-        {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
-      </p>
-    </li>
   );
 }
